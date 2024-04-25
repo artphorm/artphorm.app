5f553a0fb9cd30ff54b48fd8e6bc8466045136c4
James Johnson <jjohnson@artphorm.org>
2024-04-25 15:18:44 -0400

    Add db connection

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
+require_once __DIR__ . '/../config.php';
 
-if (isset($_POST['name'])) {
-  $name = htmlspecialchars($_POST['name']);
-  if ($name) {
-    include __DIR__ . '/../src/templates/hello.html.php';
-    exit();
-  }
+try {
+  $conn = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-
-include __DIR__ . '/../src/templates/helloform.html.php';
