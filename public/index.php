7a96ab7ddf996d294e3768c998ab3ee52896dcce
James Johnson <jjohnson@artphorm.org>
2024-04-25 17:14:09 -0400

    Display data

--- public/index.php
+++ public/index.php
@@ -4,7 +4,12 @@ require_once __DIR__ . '/../config.php';
 try {
   $conn = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4', DB_USERNAME, DB_PASSWORD);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  $sql = 'SELECT `body` from `posts`';
+  $result = $conn->query($sql);
+
+  while ($row = $result->fetch()) {
+    echo $row['body'];
+  }
 } catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
