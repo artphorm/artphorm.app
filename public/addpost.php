3fe6fc2442099084a05b92c5b36563e0b0f102be
James Johnson <jjohnson@artphorm.org>
2024-04-27 15:22:09 -0400

    Add code to create new posts

--- public/addpost.php
+++ public/addpost.php
@@ -1,6 +1,20 @@
 <?php
-ob_start();
-include __DIR__ . '/../src/templates/addpost.html.php';
-$content = ob_get_clean();
-$title = 'Add a Post';
+include_once __DIR__ . '/../src/includes/database.php';
+include_once __DIR__ . '/../src/includes/posts.php';
+
+try {
+    if (isset($_POST['body'])) {
+        $body = htmlspecialchars($_POST['body'], ENT_QUOTES, 'UTF-8');
+        $db = db_connect();
+        posts_insert($db, $body);
+        header('Location: /');
+    }
+    ob_start();
+    include __DIR__ . '/../src/templates/addpost.html.php';
+    $content = ob_get_clean();
+    $title = 'Add a Post';
+} catch (PDOException $e) {
+    $error = "Connection failed: " . $e->getMessage();
+}
+
 include __DIR__ . '/../src/templates/layout.html.php';
--- src/includes/posts.php
+++ src/includes/posts.php
@@ -8,3 +8,11 @@ function posts_getAll($db)
     }
     return $posts;
 }
+
+function posts_insert($db, $body)
+{
+    $sql = 'INSERT INTO `posts` SET `body` = :body, `date` = CURRENT_TIMESTAMP()';
+    $statement = $db->prepare($sql);
+    $statement->bindValue(':body', $body);
+    $statement->execute();
+}
